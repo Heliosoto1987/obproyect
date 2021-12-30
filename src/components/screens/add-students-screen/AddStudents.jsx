295ef91efe8dc06962c7c6b360f10463d4e6b958
Helio <heliosoto17@gmail.com>
2021-12-30 15:33:04 +0100

    new change in modal

--- src/components/screens/add-students-screen/AddStudents.jsx
+++ src/components/screens/add-students-screen/AddStudents.jsx
@@ -20,15 +20,39 @@ export const AddStudents = ({
   };
   return (
     <Modal
-      isOpen={isOpen}
+      isOpen={true}
       onRequestClose={closeModal}
       className="modal"
       overlayClassName="modal__fondo"
       closeTimeoutMS={200}
     >
-      <h1 className="modaltitle">Nuevo Alumno</h1>
-      <hr />
-      <form></form>
+      <h2 className="">Nuevo Alumno</h2>
+      <div className="add-students__flex">
+        <form className="add-students__left-form">
+          <label htmlFor="fullNameModal">Nombre y Apellidos</label>
+          <input type="text" name="fullNameModal" />
+          <label htmlFor="countryModal">Pais</label>
+          <select name="countryModal"></select>
+          <label htmlFor="cityModal">Ciudad</label>
+          <select name="cityModal"></select>
+          <label htmlFor="phoneModal">Nº Telefono</label>
+          <input type="text" name="phoneModal" />
+          <label htmlFor="emailModal">Nombre y Apellidos</label>
+          <input type="text" name="emailModal" />
+          <label htmlFor="faceModal">Presencialidad</label>
+          <input type="text" name="faceModal" />
+          <label htmlFor="transfer">Presencialidad</label>
+          <input type="text" name="transfer" />
+        </form>
+        <form>
+          <label htmlFor="">Foto de perfil</label>
+          <input type="file" />
+          <label htmlFor="">Documento CV</label>
+          <input type="file" />
+          <label htmlFor="">Etiquetas</label>
+          <input type="text" placeholder="Escribe para buscar..." />
+        </form>
+      </div>
     </Modal>
   );
 };

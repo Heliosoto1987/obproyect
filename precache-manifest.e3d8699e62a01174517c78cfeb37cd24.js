90607920f476f6c255d24eb1b756e6e29f0cbd56
Helio <heliosoto17@gmail.com>
2022-01-13 22:03:57 +0100

    Updates

---
+++ precache-manifest.e3d8699e62a01174517c78cfeb37cd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e84dc858b27e0dfa8a820cbe2a52219",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c14e9b00cf5c66dbc634",
+    "url": "/static/css/main.7bbda8b5.chunk.css"
+  },
+  {
+    "revision": "e1e49453b815d2c8e263",
+    "url": "/static/js/2.11cbc72e.chunk.js"
+  },
+  {
+    "revision": "897eb22b719834bb5b78dee71af66bba",
+    "url": "/static/js/2.11cbc72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14e9b00cf5c66dbc634",
+    "url": "/static/js/main.ea8d16b8.chunk.js"
+  },
+  {
+    "revision": "13a6dd6db5afa91005a6",
+    "url": "/static/js/runtime-main.0dc39e5a.js"
+  }
+]);
